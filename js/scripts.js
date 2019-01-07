2c8a8a7d57b2806739e18fd32b07527c7bab5776
Joan Clarke <joanclarke100@gmail.com>
2019-01-06 21:08:58 -0500

    add show-menu-link, board-wrapper, card-options-container and labels-container to sections html-css-index-page

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,33 @@
+window.onload = function() {
+  checkForBackground();
+  toggleAddListLinks();
+};
+
+function setEventListeners() {
+  document.getElementById('sample-card').addEventListener('click', (event) => { cardOptions.showOverlay(event); });
+  document.getElementById('sample-pen').addEventListener('click', (event) => {
+    event.stopPropagation();
+    cardOptions.showCardOptions(event); });
+}
+
+// setEventListeners();
 // *************** SLIDEOUT MENU SCRIPT ***************
+//Set background + background tile in menu to previous background setting
+function checkForBackground() {
+  if (localStorage.getItem('background') !== null) {
+    let background = localStorage.getItem('background');
+    menu.changeBackground(background);
+    menu.changeBackgroundIcon(background);
+  }
+}
+
 const headers = {
   'change-background': 'Change Background',
-  'filter-cards-page': 'Filter Cards',
-  'settings-page': 'Settings'
+  'colors': 'Colors',
+  'photos': 'Photos',
+  'filter-cards': 'Filter Cards',
+  'copy-board': 'Copy Board',
+  'settings': 'Settings'
 };
 
 const backgroundImages = [
@@ -13,223 +38,183 @@ const backgroundImages = [
 ];
 
 let menu = {
-  // Toggles menu from view (slide into or out of view)
-  toggleMenuContainer: function () {
+  // Toggle menu from view (slide into or out of view)
+  toggleMenuContainer: function() {
     document.getElementById('menu-container').classList.toggle("element-invisible");
     document.getElementById('show-menu-link').classList.toggle("element-invisible");
   },
-  // Shows or hides main menu
-  toggleMainMenu: function (idToHide) {
+  // Show or hide main menu
+  toggleMainMenu: function() {
+    //hide or show main menu
     document.getElementById('main-menu').classList.toggle("element-invisible");
-    document.getElementById(idToHide).classList.toggle("element-invisible");
     document.getElementById('main-menu-header').classList.toggle("element-invisible");
-    document.getElementById('menu-header').classList.toggle("element-invisible");
   },
-  // Shows or hides a specific page (page id added when function called from HTML)
-  togglePage: function (id) {
-    document.getElementById('main-menu-header').classList.toggle("element-invisible");
-    document.getElementById('menu-header').classList.toggle("element-invisible");
-    document.getElementById('page-header').innerHTML = headers[id];
-    document.getElementById(id).classList.toggle("element-invisible");
+  //hide all pages (before showing selected page)
+  hideAllPages: function() {
+    for (let key in headers) {
+      if (document.getElementById(key).classList.contains('element-invisible') !== true) {
+      document.getElementById(key).classList.toggle('element-invisible');
+      }
+    }
+  },
+  // Show or hide a specific page (page id passed as argument when function called from HTML)
+  togglePage: function(id) {
+    //hide main menu and all pages
     menu.toggleMainMenu();
+    menu.hideAllPages();
+    //hide or show header for current page
+    document.getElementById('menu-header').classList.toggle("element-invisible");
+    //set header text to match current page
+    document.getElementById('page-header').innerHTML = headers[id];
+    //show selected page
+    document.getElementById(id).classList.toggle("element-invisible");
   },
-  // Changes background color
-  changeBackground: function (color) {
-    document.body.style.background = color;
+  // Change background color
+  changeBackground: function(background) {
+    localStorage.setItem('background', background);
+    if (background.startsWith('rgb')) {
+      document.body.style.background = background;
+    }
+    else {
+      document.body.style.background = "url("+ background +")";
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundRepeat = "no-repeat";
+    }
+    menu.changeBackgroundIcon(background);
   },
-  // Changes background image
-  changeBackgroundImage: function (imageURL) {
-    document.body.style.backgroundImage = "url(" + imageURL + ")";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundRepeat = "no-repeat";
+  changeBackgroundIcon: function(background) {
+    let backgroundIcon = document.getElementById('background-menu-icon');
+    if (background.startsWith('rgb')) {
+      backgroundIcon.style.background = background;
+    }
+    else {
+      backgroundIcon.style.background = "url("+ background +")";
+    }
   }
 };
 
-// *************** ADD LIST **************
+// *************** ADD LISTS ***************
 
-// Creates a title textarea
-function displayTitle() {
-  var listTitle = document.getElementById("listTitle");
+// add new list submit eventlistener
+document.getElementById("add-list-form").addEventListener("submit", addList);
 
-  var titleTextArea = document.createElement("textarea");
-  var listTitleExist = true;
-  if (titleTextArea === null) {
-    titleTextArea = document.createElement("textarea");
-    titleTextArea.id = "listTitleArea";
-    // adds the newly created element to the DOM
-    listTitleExist = false;
+//Declaring index
+let listIndex = 0;
+let countCard = 0;
+function addList(e) {
+  e.preventDefault();
+  const input = document.getElementById("list-name");
+  const name = input.value;
+  input.value = '';
+  if ('' == name) {
+    return;
   }
-  titleTextArea.setAttribute("type", "text");
-  titleTextArea.setAttribute("id", "textAreaTitle2");
-  titleTextArea.setAttribute("overflow", "break-word");
-  titleTextArea.setAttribute("placeholder", "Enter list title...");
-  listTitle.appendChild(titleTextArea);
-  var titleTaker = document.getElementById("textAreaTitle").value;
-  titleTextArea.innerHTML = titleTaker;
-  document.getElementById("textAreaTitle").value = "";
+
+  // create list
+  const list = document.createElement('div');
+  list.setAttribute('class', 'list');
+  list.innerHTML =
+    `<div class="list-container">
+    <div class="list-heading" >
+      <h3 contenteditable="true">` + name + `</h3>
+    <div class= "ellipsis"><a href="#">&#8230;</a></div>
+    </div>
+      <div> 
+        <form class="add-item-form">
+          <textarea placeholder="Enter a title for this card..."></textarea>
+          <div>
+          <input type="submit" value="Add Card">
+          <input type="button" value="&#88;" onclick="hideSHowForm(this,'add-item-form', 'show-card-form', ` + listIndex + `);">
+          <div class= "ellipsis"><a href="#">&#8230;</a></div>
+          </div>
+        </form>
+      </div>
+      <div class="link-wrapper">
+      <a href="#" class="show-card-form" onclick="hideSHowForm(this,'add-item-form', 'show-card-form', ` + listIndex + `);">
+      <span class="placeholder"><i class="fas fa-plus"></i> Add a card</span>
+      <span class="placeholder"><i class="fas fa-plus"></i> Add another card</span>
+    </a>
+    </div>
+      </div>`;
+
+   //Increasing index
+   listIndex++
+  document.getElementById("list-wrapper").appendChild(list);
 }
 
-function displayListInputField() {
-  //checks to see if there is a div#listTextArea in our DOM (null means we do not have div#inputField), and creates it if its not there
-  var textAreaHolder = document.getElementById("listTextArea");
-  var listTextAreaExist = true;
-  if (textAreaHolder === null) {
-    textAreaHolder = document.createElement("listTextArea");
-    textAreaHolder.id = "listTextArea";
-    listTextAreaExist = false;
-  }
-
-  // Creates a textarea for adding lists
-  var textAreaElement = document.createElement("textarea");
-  textAreaElement.setAttribute("type", "text");
-  textAreaElement.setAttribute("id", "textAreaTitle");
-  textAreaElement.setAttribute("overflow", "break-word");
-  textAreaElement.setAttribute("placeholder", "Enter list title...");
-  textAreaHolder.appendChild(textAreaElement);
-
-  // Creates a "Add a List" button. It adds the textarea when clicked
-  var button = document.createElement("button");
-  button.innerHTML = "Add List";
-  button.setAttribute("id", "createNewList");
-  textAreaHolder.appendChild(button);
-
-  button.addEventListener("click", function () {
-    var titleTaker = document.getElementById("textAreaTitle").value;
-    if (titleTaker == "") {
-      titleTaker = false;
-    } else {
-      showTitleAndCardSection(); //Displays the List Container
-      displayTitle(); // Adds a title
-      hideButton('textAreaTitle');//hide Textarea
-      hideButton('createNewList');//hide 'Add a List' button
-      hideButton('createNewCloseBtn');//hide close 'x' button
-      // createNewListTextArea();
-      // duplicate();
+// add new item submit eventlistener
+document.addEventListener('submit', function(e) {
+  if (e.target.matches('.add-item-form')) {
+    e.preventDefault();
+    const textarea = e.target.getElementsByTagName('textarea')[0];
+    const text = textarea.value;
+    textarea.value = '';
+    if ('' == text) {
+      return;
     }
-  });
-
-  // Creates a delete "x" button and set attributes to it
-  var closeButton = document.createElement("closeButton");
-  closeButton.innerHTML = '<i class="fas fa-times"></i>';
-  closeButton.setAttribute("id", "createNewCloseBtn");
-  textAreaHolder.appendChild(closeButton);
-
-  //Checks to see if a div#listTextArea exists and creates one if it does not exist and appends it to its parentNode
-  if (!listTextAreaExist) {
-    document.querySelector(".listTextAreaContainer").appendChild(textAreaHolder);
+    //create card
+    const cardItem = document.createElement('p');
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+    //create pen icon
+    const pen = document.createElement('a');
+    pen.innerHTML = '<i class="fas fa-pen"></i>';
+    pen.addEventListener('click', function(event){
+      cardOptions.setCurrentCard(event);
+      cardOptions.showCardOptions(event);
+    });
+    cardItem.innerHTML = text;
+    card.appendChild(cardItem)
+    card.appendChild(pen);
+    e.target.parentElement.prepend(card);
+    //Increasing countCard
+    countCard++;
   }
-  // adds an event listener which will call the removeList() function to delete the div#listTextAreaExist when clicked
-  closeButton.addEventListener("click", function () {
-    removeList();
-  });
-}
+});
 
-//Shows the Title and Add Cards sections
-function showTitleAndCardSection() {
-  var showCardSection = document.getElementsByClassName("listContainer");
-  for (var i = 0; i < showCardSection.length; i += 1) {
-    showCardSection[i].style.display = 'block';
+let spans = document.getElementsByClassName("placeholder");
+//toggle between 'add a list' and 'add another list' links
+function toggleAddListLinks() {
+   spans[1].style.display='none';
+   document.forms[0].style.display='none';
+};
+
+let clicked = 0;
+//toggle between links and 'add-list-form'
+function toggleDiv(formId, linkId){
+  clicked++;
+  if(document.getElementById( formId ).style.display == 'block'){
+    document.getElementById( formId ).style.display = 'none';
+    document.getElementById( linkId ).style.display = 'block';
+  }else{	
+    document.getElementById( linkId ).style.display = 'none';
+    document.getElementById( formId ).style.display = 'block'
+  }if(clicked > 0) {
+    spans[0].style.display='none';
+    spans[1].style.display='block';
   }
 }
 
-function removeList(listTextArea) {
-  var element = document.getElementById("listTextArea");
-  var emptyList = document.getElementById("textAreaTitle").value === "";
-  if (emptyList) {
-    document.getElementById("addListLink").style.display = "block";
+document.getElementsByClassName('')
+
+//toggle between links and 'add-item-form'
+function hideSHowForm(curr, form, link, id) {
+  //show only one 'add-item-form' Form on screen and hide other form
+  let allCard = document.querySelectorAll('.add-item-form');
+  allCard.forEach(el => el.style.display = 'none');
+  document.querySelectorAll('.show-card-form').forEach(a => a.style.display = 'block');
+  curr.parentNode.parentNode.style.display = 'block';
+
+  if (document.getElementsByClassName(form)[id].style.display == 'block') {
+    document.getElementsByClassName(form)[id].style.display = 'none';
+    document.getElementsByClassName(link)[id].style.display = 'block';
+  } else {
+    document.getElementsByClassName(link)[id].style.display = 'none';
+    document.getElementsByClassName(form)[id].style.display = 'block'
+  } if (countCard > 0) {
+    spans[0].style.display = 'none';
+    spans[1].style.display = 'block';
   }
-  // else{
-  //   document.getElementById("addAnotherListLink").style.display = "block";
-  // }
-  element.parentNode.removeChild(element);
-}
-
-function createNewListTextArea() {
-  var listWrapper = document.getElementById("listWrapper");
-  var newListContainerDiv = document.createElement("div");
-  newListContainerDiv.setAttribute("id", "newListDiv");
-  listWrapper.appendChild(newListContainerDiv);
-}
-
-
-
-// function duplicate() {
-//   var i=0;
-//   var original = document.getElementById("listTextArea");
-//   var clone = original.cloneNode(true);
-//   clone.id = "listTextArea" + ++i;
-//   original.parentNode.appendChild(clone);
-// }
-
-// *************** ADD - DELETE - EDIT CARDS  **************
-
-/*** Creates an inputField which contains a textarea, a 'Add a Card' button and a delete 'X' button.***/
-function displayInputField() {
-  //checks to see if there is a div#inputField in our DOM (null means we do not have div#inputField), and creates it if its not there
-  var a = document.getElementById("inputField");
-  var inputFieldExist = true;
-  if (a === null) {
-    a = document.createElement("inputField");
-    a.id = "inputField";
-    // adds the newly created element to the DOM
-    inputFieldExist = false;
-  }
-
-  // Creates a textarea for input
-  var b = document.createElement("textarea");
-  b.setAttribute("type", "text");
-  b.setAttribute("id", "userInput");
-  b.setAttribute("overflow", "break-word");
-  b.setAttribute("placeholder", "Enter a title for this card...");
-  a.appendChild(b);
-
-  // Creates a "Add a Card" button. It adds the textarea when clicked
-  var button = document.createElement("button");
-  button.innerHTML = "Add Card";
-  button.setAttribute("id", "createNewCard");
-  a.appendChild(button);
-
-  //Call the CreateCard() function to create a new card when the 'Add a Card' button is clicked.
-  button.addEventListener("click", function () {
-    //Checks to see if the textarea is empty. If it is a card will not be created when the user clicks the 'Add a Card' button.
-    if (document.getElementById("userInput").value === "") {
-      return false;
-    } else {
-      createACard();
-    }
-  });
-
-  // Creates a delete "x" button and set attributes to it
-  var closeButton = document.createElement("closeButton");
-  closeButton.innerHTML = '<i class="fas fa-times"></i>';
-  closeButton.setAttribute("id", "createNewCloseBtn");
-  a.appendChild(closeButton);
-
-  //Checks to see if a div#inputField exists and creates one if it does not exist and appends it to its parentNode
-  if (!inputFieldExist) {
-    document.querySelector(".cardContainer").appendChild(a);
-  }
-  // adds an event listener which will call the removeCard () function to delete the div#inputField when clicked
-  closeButton.addEventListener("click", function () {
-    removeCard();
-  });
-}
-
-//Hides the 'Add a Card' and 'Add another card' links when they are clicked.
-function hideButton(x) {
-  document.getElementById(x).style.display = "none"; // hide the button
-}
-
-//Takes the input from div#inputField and creates a new 'titled' card
-function createACard() {
-  var createCardElem = document.getElementById("createCard");
-  var createNewCard = document.createElement("div");
-  createNewCard.setAttribute("id", "newCard");
-  createCardElem.appendChild(createNewCard);
-  var inputTaker = document.getElementById("userInput").value;
-  //appending the user's input to the new card
-  createNewCard.innerHTML = inputTaker;
-  document.getElementById("userInput").value = ""; //empties the text-area after 'Add a Card' button is clicked.
 }
 
 //removes the inputField and buttons from the document and shows the 'addAnotherCardLink' after it has been clicked.
@@ -237,4 +222,86 @@ function removeCard(inputField) {
   var element = document.getElementById("inputField");
   element.parentNode.removeChild(element);
   document.getElementById("addAnotherCardLink").style.display = "block";
+}
+
+
+
+// *************** CARD OPTIONS (small + modal) ***************
+// declare array for label colors (cardOptions.changeLabel())
+let labelArr = [];
+let cardOptions = {
+  currentCard: null,
+  setCurrentCard: function(pen) {
+    this.currentCard = this.getDiv(pen.target, 'card');
+  },
+  getDiv: function (elem, divClass) {
+    for ( ; elem && elem !== document; elem = elem.parentNode ) {
+      if (elem.classList.contains(divClass)) {
+        return elem;
+     }
+    }
+    return null;
+  },
+  showOverlay: function() {
+    document.getElementById('overlay-container').classList.toggle('element-invisible');
+    document.body.classList.add('ovleray-opacity');
+  },
+  showCardOptions: function(event) {
+    let target = event.target;
+    // Get parent elements with 'list' and 'card' class names
+    let list = cardOptions.getDiv(target, 'list');
+    let card = cardOptions.getDiv(target, 'card');
+    // Get position of list and card
+    let listPosition = list.getBoundingClientRect();
+    let cardPosition = card.getBoundingClientRect();
+    let cardOptionsContainer = document.getElementById('card-options-container');
+    cardOptionsContainer.classList.toggle('element-invisible');
+    let labelsContainer = document.getElementById('labels-container');
+    if (!labelsContainer.classList.contains('element-invisible')) {
+      labelsContainer.classList.toggle('element-invisible');
+    }
+    // Set card options container to be flush with right edge of list
+    cardOptionsContainer.style.left = listPosition.left + list.offsetWidth + "px";
+    // Set card options container to be flush with top of card
+    cardOptionsContainer.style.top = cardPosition.bottom - card.offsetHeight -7 + "px";
+
+  },
+// *************** SHOW X OPTION ***************
+  showLabels: function(event) {
+    let labelsContainer = document.getElementById('labels-container');
+    labelsContainer.classList.toggle('element-invisible');
+    // labelsContainer.style.left = event.target.getBoundingClientRect().left;
+  },
+
+// *************** ADD MEMBER OPTION ***************
+// *************** ADD/CHANGE LABEL ***************
+  changeLabel: function(event, labelColor) {
+    // NEXT STEP: Must access each unique card with a unique ID. Then, can set an individual labelArr for each card.
+    let card = this.currentCard;
+      //check if color is already displayed (i.e. in the array)
+    let currentLabels = card.getElementsByClassName('card-label-tile');
+    if (labelColor in currentLabels) {
+      card.removeChild(document.getElementById(labelColor));
+      // labelArr.splice(this.card.labelArr.indexOf(labelColor),1);
+    }
+      // if color not already displayed, dispaly it
+    else {
+      //create new HTML element to display color label
+      let label = document.createElement('div');
+      label.classList.add('card-label-tile');
+      label.id = labelColor;
+      label.style.backgroundColor = labelColor;
+      // add new element (color label) to card
+      card.appendChild(label);
+    }
+  },
+
+// *************** ADD CHECKLIST OPTION ***************
+  addChecklist: function() {
+
+  },
+// *************** ADD DUE DATE OPTION ***************
+  addDueDate: function() {
+
+  }
 }
